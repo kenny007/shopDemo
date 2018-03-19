46e72be4f3f70b8c3c31f5e247e41883a3fb2493
Kehinde Adebayo <press.spark@gmail.com>
2018-03-19 04:03:01 +0200

    Adds product service, dependency Injection and Observables

--- src/app/shared/convert-to-spaces.pipe.ts
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name:'nameToSpaces'
+    name:'convertToSpaces'
 })
 
 export class ConvertToSpacesPipe implements PipeTransform {
